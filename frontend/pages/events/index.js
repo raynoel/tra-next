157f39126837d3f8753f5ca51505795de1c0b633
nic <xfri3nd@gmail.com>
2021-12-15 18:49:07 -0500

    Pagination

--- frontend/pages/events/index.js
+++ frontend/pages/events/index.js
@@ -1,24 +1,29 @@
 import axios from 'axios'
 import Layout from "../../components/Layout.jsx"
 import EventItem from '../../components/EventItem.jsx'
-import { API_URL } from '../../config/index.js'
+import Pagination from '../../components/Pagination.jsx'
+import { API_URL, PER_PAGE } from '../../config/index.js'
 
 
-export default function EventPage({ events }) {
+export default function EventPage({ events, page, total }) {
   return (
     <Layout>
       <h1>Events</h1>
       {events.lenght === 0 && <h3>No Events to show</h3>}
       {events.map((evt) => <EventItem key={evt.id} evt={evt} />)}
+      <Pagination page={page} total={total} />
     </Layout>
   )
 }
 
-// Obtient les données de la DB
-export async function getStaticProps() {
-  const {data: events} = await axios.get(`${API_URL}/events?_sort=date:ASC`)
+// Pagination + Obtient les données de la DB
+export async function getServerSideProps({ query: { page = 1 }}) {                     // obtient ?page= du URL
+  // Calcul le start
+  page = parseInt(page)
+  const start = page === 1 ? 0 : (page - 1) * PER_PAGE;
+  const {data: total}  = await axios.get(`${API_URL}/events/count`);
+  const {data: events} = await axios.get(`${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`)
   return {
-    props: { events },
-    revalidate: 1                      // Option pour simuler un observer, refait la requête après 1 seconde si la DB fut modifiée
+    props: { events, page, total },
   }
 }
