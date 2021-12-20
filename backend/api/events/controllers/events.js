8e20182adbb1dd084628edb87766acf42c3a8724
nic <xfri3nd@gmail.com>
2021-12-20 05:35:24 -0500

    0801 API qui retourne la liste des 'events' ajoutés par l'usagé

--- backend/api/events/controllers/events.js
+++ backend/api/events/controllers/events.js
@@ -1,79 +1,28 @@
 "use strict";
-const { sanitizeEntity } = require("strapi-utils");
+const { sanitizeEntity } = require("strapi-utils");               // prévient les injection JS
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
-
-module.exports = {
   // Create event with linked user
   // https://strapi.io/documentation/developer-docs/latest/guides/is-owner.html
-  async create(context) {
-    let entity;
-    if (context.is("multipart")) {
-      const { data, files } = parseMultipartData(context);
-      entity = await strapi.services.events.create(data, { files });
-    } else {
-      // context.request.body.user = context.state.user.id;
-      entity = await strapi.services.events.create(context.request.body);
-    }
-    return sanitizeEntity(entity, { model: strapi.models.events });
-  },
+module.exports = {
 
-  // Update event
-  async update(context) {
-    const { id } = context.params;
-    let entity;
-    const [events] = await strapi.services.events.find({
-      id: context.params.id,
-      // "user.id": context.state.user.id,
-    });
-    if (!events) {
-      return context.unauthorized(`You can't update this entry`);
-    }
-    if (context.is("multipart")) {
-      const { data, files } = parseMultipartData(context);
-      entity = await strapi.services.events.update({ id }, data, { files});
-    } else {
-      entity = await strapi.services.events.update({ id }, context.request.body );
-    }
-    return sanitizeEntity(entity, { model: strapi.models.events });
-  },
-
-
-  // Delete an event
-  async delete(context) {
-    const { id } = context.params;
-    const [events] = await strapi.services.events.find({
-      id: id,
-      // 'author.id': context.state.user.id,
-    });
-    if (!events) {
-      return context.unauthorized(`You can't delete this entry`);
-    }
-    const entity = await strapi.services.events.delete({ id });
-    return sanitizeEntity(entity, { model: strapi.models.events });
-  },
-
-
-
-  // Get logged in users
+  // Retourne la liste des 'events' ajoutés par l'usagé identifié par son token
   async me(context) {
-    const user = context.state.user;
-
+    const user = context.state.user;                                      // Obtient l'usagé identifié par son token
     if (!user) {
       return context.badRequest(null, [
         { messages: [{ id: "No authorization header was found" }] },
       ]);
     }
 
-    const data = await strapi.services.events.find({ user: user.id });
-
-    if (!data) {
-      return context.notFound();
-    }
+    const data = await strapi.services.events.find({ user: user.id });    // Obtient la liste des 'events' de l'usagé
+    if (!data) { return context.notFound(); }
 
     return sanitizeEntity(data, { model: strapi.models.events });
   },
+
+
 };
